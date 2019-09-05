36f0153ea6f82eceb3e5f632e447a26014a6f508
sdp <403213485@qq.com>
2019-09-05 23:09:34 +0800

    debug 11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,40 @@
 
 <template>
   <div class="home">
-     <h1>包裹入库</h1>
-       <div>
-       <span>运单号：</span><input v-model="orderNumber" type="text">
-    </div>     
-    <h1></h1>
-      <div>
-       <span>收件人：</span><input v-model="reciverName" type="text">
-    </div> 
-     <h1></h1>
-      <div>
-       <span>电话：</span><input v-model="reciverTel" type="text">
-    </div> 
-     <h1></h1>
-      <div>
-       <span>重量：</span><input v-model="weight" type="text">
+    <h1>包裹入库</h1>
+    <div>
+      <span>运单号：</span>
+      <input v-model="orderNumber" type="text" />
     </div>
     <h1></h1>
-     <div>
-       <button @click="msg1Call" >提交</button>
-    </div>  
+    <div>
+      <span>收件人：</span>
+      <input v-model="reciverName" type="text" />
+    </div>
+    <h1></h1>
+    <div>
+      <span>电话：</span>
+      <input v-model="reciverTel" type="text" />
+    </div>
+    <h1></h1>
+    <div>
+      <span>重量：</span>
+      <input v-model="weight" type="text" />
+    </div>
+    <h1></h1>
+    <div>
+      <button @click="msg1Call">提交</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-   
-  },
+  name: "home",
+  components: {},
 
   // data(){
   //    return {
@@ -41,22 +43,26 @@ export default {
   //    reciverTel:'',
   //    weight:''
   //           }
-    
-  //  },
-     data:{
-    
-     orderNumber:'',
-     reciverName:'',
-     reciverTel:'',
-     weight:''
 
-   },
-   methods: {
+  //  },
+  data: {
+    orderNumber: "",
+    reciverName: "",
+    reciverTel: "",
+    weight: ""
+  },
+  methods: {
     msg1Call: function() {
       let self = this;
-      axios.post('http://localhost:8080/',{orderNumber:this.orderNumber,reciverName:this.reciverName,reciverTel:this.reciverTel,weight:this.weight}).then(function(e){
-      })
+      axios
+        .post("http://localhost:8080/", {
+          orderNumber: this.orderNumber,
+          reciverName: this.reciverName,
+          reciverTel: this.reciverTel,
+          weight: this.weight
+        })
+        .then(function(e) {});
     }
   }
-}
+};
 </script>
